6347f42d01a3266e59f8ad22abe715ad32889bcf
insino <dongkwan1114@kaist.ac.kr>
2011-01-25 20:55:44 +0900

    Third commit

--- spec/fib_spec.rb
+++ spec/fib_spec.rb
@@ -1,8 +1,13 @@
 require 'fib'
 describe Fib  do
+	before do
+		@fib = Fib.new
+	end
 	it "should retturn 1 with argument 1" do
-		fib = Fib.new
-		fib.get(1) == 1
+		@fib.get(1).should == 1
+	end
+	it "should retturn 1 with argument 2" do
+		@fib.get(2).should == 1
 	end
 end
 
