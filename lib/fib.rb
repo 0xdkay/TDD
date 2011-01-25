171f8d0f0d7b9b4572c110c1075ad58cbec33503
insino <dongkwan1114@kaist.ac.kr>
2011-01-25 21:00:57 +0900

    Final commit

--- lib/fib.rb
+++ lib/fib.rb
@@ -1,6 +1,7 @@
 class Fib
-	def get(aa)
-		return 1
+	def get(n)
+	return 1 if n==1 || n==2
+	return get(n-1) + get(n-2)
 	end
 end
 
--- spec/fib_spec.rb
+++ spec/fib_spec.rb
@@ -9,5 +9,14 @@ describe Fib  do
 	it "should retturn 1 with argument 2" do
 		@fib.get(2).should == 1
 	end
+	it "should retturn 2 with argument 3" do
+		@fib.get(3).should == 2
+	end
+	it "should retturn 3 with argument 4" do
+		@fib.get(4).should == 3
+	end
+	it "should retturn 55 with argument 10" do
+		@fib.get(10).should == 55
+	end
 end
 
